803bb8b92964c786f7a33c4967d9e80a80bb1d25
Sara Dawidziuk <bsmykowski@wp.pl>
2017-12-15 18:56:38 +0100

    Day 10 done

--- adventOfCode/src/year2017/Day10/Day10.java
+++ adventOfCode/src/year2017/Day10/Day10.java
@@ -11,31 +11,56 @@ public class Day10 {
         String path = "src/year2017/Day10/input.txt";
         List<String> lines = FilesReader.getLines(path);
 
-        List<Integer> lenghts = new ArrayList<>();
-
-        for(String line : lines){
-            String[] tokens = line.split(",");
-            for(int i = 0; i < tokens.length; i++){
-                lenghts.add(Integer.parseInt(tokens[i]));
-            }
-        }
+        List<Integer> lengths = convertToLengths(lines);
 
         HashList hashList = new HashList(256);
         HashListPrinter hashListPrinter = new HashListPrinter(hashList);
         hashListPrinter.printWithMarkedCurrentIndex();
-        for(Integer length:lenghts){
-            hashList.doStep(length);
-            System.out.println(length);
-            hashListPrinter.printWithMarkedCurrentIndex();
+
+        for(int i = 0; i <64;i++) {
+            for (Integer length : lengths) {
+                hashList.doStep(length);
+            }
         }
 
-        System.out.println();
-        int first = hashList.get(0);
-        int second = hashList.get(1);
-        System.out.println(first * second);
-        hashListPrinter.printWithMarkedCurrentIndex();
+        List<Integer> hashes = new ArrayList<>();
+        for(int i = 0; i <16;i++) {
+            int hash = hashList.getList().get(i * 16);
+            for (int j = 1; j < 16; j++) {
+                hash ^= hashList.getList().get(i * 16 + j);
+            }
+            hashes.add(hash);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int hash:hashes) {
+            if(hash < 16){
+                stringBuilder.append('0');
+            }
+            stringBuilder.append(Integer.toHexString(hash));
+        }
+
+
+        System.out.println(stringBuilder.toString());
     }
 
+    private static List<Integer> convertToLengths(List<String> lines) {
+        List<Integer> lengths = new ArrayList<>();
+
+        for(String line : lines){
+            for(int i = 0; i < line.length(); i++){
+                char character = line.charAt(i);
+                int ASCIICode = (int)character;
+                lengths.add(ASCIICode);
+            }
+        }
+        lengths.add(17);
+        lengths.add(31);
+        lengths.add(73);
+        lengths.add(47);
+        lengths.add(23);
+        return lengths;
+    }
 
 
 }
