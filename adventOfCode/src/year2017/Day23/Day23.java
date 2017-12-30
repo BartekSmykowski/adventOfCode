9fef206d3df46e04a8b9d270775a1d4cf41e3191
janusz96 <bsmykowski@wp.pl>
2017-12-30 02:08:50 +0100

    Day23

--- adventOfCode/src/year2017/Day23/Day23.java
+++ adventOfCode/src/year2017/Day23/Day23.java
@@ -3,7 +3,7 @@ package year2017.Day23;
 public class Day23 {
 
     public static void main(String[] args) {
-        program();
+        program2();
     }
 
     public static void program(){
@@ -45,4 +45,32 @@ public class Day23 {
 
     }
 
+    public static void program2(){
+        long b = 108100;
+        long h = 0;
+
+        long time = System.currentTimeMillis();
+
+        while(true) {
+            boolean prime = isPrime(b);
+            System.out.println(b + ": " + prime + " " + (System.currentTimeMillis() - time));
+            if (!prime)
+                h++;
+            if (b == 125100) {
+                System.out.println(h);
+                return;
+            }
+            b += 17;
+        }
+    }
+
+    private static boolean isPrime(long number){
+        long divider = 2;
+        while(divider < number){
+            if(number%divider == 0)
+                return false;
+            divider++;
+        }
+        return true;
+    }
 }
---
+++ adventOfCode/src/year2017/Day24Domino/Day24.java
@@ -0,0 +1,17 @@
+package year2017.Day24Domino;
+
+import util.FilesReader;
+
+import java.util.List;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        String filePath = "src/year2017/Day24Domino/input.txt";
+        List<String> lines = FilesReader.getLines(filePath);
+
+
+        
+    }
+
+}
